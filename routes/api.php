d4adf585d0cafa68bf3e0c3a099c0d2da7e60e4e
kalpana udara <kalpanaudara058@gmail.com>
2025-03-23 11:22:58 +0530

    Created user sign in endpoint.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function () {
+Route::post('/sign-in', function () {
     return ['hello' => 'world'];
 });
